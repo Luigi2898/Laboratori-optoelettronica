0cc1c8e66eeba8b69429b4f0c561d9b6194ed2d2
Luigi2898 <32927727+Luigi2898@users.noreply.github.com>
2020-01-20 14:41:26 +0100

    Update quantum_well.m

--- Laboratorio 1/quantum_well.m
+++ Laboratorio 1/quantum_well.m
@@ -9,22 +9,26 @@ m_h = 3.9351705e-31; %kg
 h_t = 6.582199e-16; %eV*s
 U1e = 0; %eV
 U2e = 0.162; %eV
-Ee = linspace(U1e, U2e, 10000);
+Ee = linspace(U1e, U2e, 1000000);
 gammae = (sqrt(2*m_e)./h_t).*sqrt(U2e - Ee);
 deltae = (sqrt(2*m_e)./h_t).*sqrt(U2e - Ee);
 k_xe = (sqrt(2*m_e)./h_t).*sqrt(Ee - U1e);
 d0e = (1./k_xe).*(atan(gammae./k_xe)+ atan(deltae./k_xe)); %nm
+d_TE = (atan(gammae./k_xe) + atan(deltae./k_xe) + pi)./(k_xe);
 d1e = (1./k_xe).*(atan(gammae./k_xe)+ atan(deltae./k_xe)+pi); %nm
 
 
 U1h = 0.108; %eV
 U2h = 0; %eV
-Eh = linspace(U2h, U1h, 10000);
+Eh = linspace(U2h, U1h, 1000000);
 gammah = (sqrt(2*m_h)./h_t).*sqrt(Eh - U2h);
 deltah = (sqrt(2*m_h)./h_t).*sqrt(Eh - U2h);
 k_xh = (sqrt(2*m_h)./h_t).*sqrt(U1h - Eh);
 d0h = (1./k_xh).*(atan(gammah./k_xh)+ atan(deltah./k_xh)); %nm
 d1h = (1./k_xh).*(atan(gammah./k_xh)+ atan(deltah./k_xh)+pi); %nm
+
+x = 20 * ones(1, 50);
+y = linspace(0, 0.18, 50);
 
 figure(11)
 plot(d0e,Ee,'r',d1e,Ee,'b')
@@ -38,10 +42,15 @@ title('E vs d - LACUNE')
 grid on
 xlim([0 100])
 
-d = 10e-9;
-E1e = 2.0875e-20;
-E0e = 5.8665e-21;
-E1h = -1.2553e-19;
-E0h = -1.2847e-19;
+figure(33)
+plot(d0h,Eh,'r',d1h,Eh,'b', d0e,Ee, 'g', d1e, Ee, 'm', x, y, '.-')
+title('E vs d - LACUNE')
+grid on
+xlim([0 100])
 
-lambda_ric = h_t*2*pi*3e8/(E0e-E0h);
+E0e = 0.048556; %eV
+E1e = 0.155420; %eV
+E1h = 0.072684; %eV
+E0h = 0.099008; %eV
+
+lambda_ric = h_t*2*pi*3e8/(E0e+E0h+0.775437);
