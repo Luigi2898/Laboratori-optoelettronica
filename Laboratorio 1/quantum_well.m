cc5046f39f8a6d260ed6d1ac1bbceb0acbd96b60
Luigi2898 <32927727+Luigi2898@users.noreply.github.com>
2020-01-20 12:12:23 +0100

    j

--- Laboratorio 1/quantum_well.m
+++ Laboratorio 1/quantum_well.m
@@ -1,35 +1,37 @@
-clear all
-close all
-clc
+%clear all
+%close all
+%clc
 
-m_e = 4.385358e-32;
-m_h = 3.9351705e-31;
-h_t = 6.582199e-16*1.60217e-19;
-U1e = 0;
-U2e = 0.162*1.60217e-19;
+%TODO plot diagramma a bande
+
+m_e = 4.385358e-32; %kg
+m_h = 3.9351705e-31; %kg
+h_t = 6.582199e-16; %eV*s
+U1e = 0; %eV
+U2e = 0.162; %eV
 Ee = linspace(U1e, U2e, 10000);
 gammae = (sqrt(2*m_e)./h_t).*sqrt(U2e - Ee);
 deltae = (sqrt(2*m_e)./h_t).*sqrt(U2e - Ee);
 k_xe = (sqrt(2*m_e)./h_t).*sqrt(Ee - U1e);
-d0e = (1./k_xe).*(atan(gammae./k_xe)+ atan(deltae./k_xe));
-d1e = (1./k_xe).*(atan(gammae./k_xe)+ atan(deltae./k_xe)+pi);
+d0e = (1./k_xe).*(atan(gammae./k_xe)+ atan(deltae./k_xe)); %nm
+d1e = (1./k_xe).*(atan(gammae./k_xe)+ atan(deltae./k_xe)+pi); %nm
 
 
-U1h = (-0.7-0.108)*1.60217e-19;
-U2h = -0.7 * 1.60217e-19;
-Eh = linspace(U1h, U2h, 10000);
-gammah = (sqrt(2*m_h)./h_t).*sqrt(U2h - Eh);
-deltah = (sqrt(2*m_h)./h_t).*sqrt(U2h - Eh);
-k_xh = (sqrt(2*m_h)./h_t).*sqrt(Eh - U1h);
-d0h = (1./k_xh).*(atan(gammah./k_xh)+ atan(deltah./k_xh));
-d1h = (1./k_xh).*(atan(gammah./k_xh)+ atan(deltah./k_xh)+pi);
+U1h = 0.108; %eV
+U2h = 0; %eV
+Eh = linspace(U2h, U1h, 10000);
+gammah = (sqrt(2*m_h)./h_t).*sqrt(Eh - U2h);
+deltah = (sqrt(2*m_h)./h_t).*sqrt(Eh - U2h);
+k_xh = (sqrt(2*m_h)./h_t).*sqrt(U1h - Eh);
+d0h = (1./k_xh).*(atan(gammah./k_xh)+ atan(deltah./k_xh)) .* 40.02499; %nm
+d1h = (1./k_xh).*(atan(gammah./k_xh)+ atan(deltah./k_xh)+pi) .* 40.02499; %nm
 
-figure(1)
+figure(11)
 plot(d0e,Ee,'r',d1e,Ee,'b')
 title('E vs d - ELETTRONI')
 grid on
 
-figure(2)
+figure(22)
 plot(d0h,Eh,'r',d1h,Eh,'b')
 title('E vs d - LACUNE')
 grid on
