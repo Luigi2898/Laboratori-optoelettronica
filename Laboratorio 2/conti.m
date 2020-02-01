652f417c9f401decfffe1f87dd2e118d33170e8d
Luigi <luigi.giuffrida.98@gmail.com>
2020-02-01 18:07:14 +0100

    add

--- Laboratorio 2/conti.m
+++ Laboratorio 2/conti.m
@@ -99,8 +99,8 @@ semilogx(x6, y6, 'g')
 semilogx(x10, y10, 'y')
 legend('I_{bias}/I_{th} = 1.05','I_{bias}/I_{th} = 2','I_{bias}/I_{th} = 6','I_{bias}/I_{th} = 10')
 title('Confronto tra le risposte IM per diverse correnti di bias')
-xlabel('Risposta IM [dB]')
-ylabel('Frequenza [GHz]')
+ylabel('Risposta IM [dB]')
+xlabel('Frequenza [GHz]')
 
 %banda come funzione della corrente di bias
 f1_05_3dB = x1_05(20);
@@ -122,7 +122,64 @@ v_g = c/n_g;
 tau_p = 1/(v_g * (alpha_i_fix * 10^2 + alpha_m_fix * 10^2));
 banda_max_ideale = sqrt(2)/(tau_p * 2 * pi);
 
-%TODO Calcolare la banda teorica date le diverse correnti di bias e confrontare con il risultato delle simulazioni
+one_ov_tau_Dn = 2 * B * N_th + A + 3 * C * N_th;
+Np = 0; %FIXME Calcolare la densità di fotoni come funzione della corrente di bias
+damping = v_g * a_0 * Np + one_ov_tau_Dn;
 
-%DOING
+%esercizio 3.4 (ricalcolo con epsilon = 1.5e-14)
+h1_05 = openfig('esercizio_3_punto_4__1_05_IM.fig');
+t1_05 = findobj(h1_05,'type', 'line');
 
+x1_05 = get(t1_05,'XData');
+y1_05 = get(t1_05,'YData');
+
+h2 = openfig('esercizio_3_punto_4__2_IM.fig');
+t2 = findobj(h2,'type', 'line');
+
+x2 = get(t2,'XData');
+y2 = get(t2,'YData');
+
+h6 = openfig('esercizio_3_punto_4__6_IM.fig');
+t6 = findobj(h6,'type', 'line');
+
+x6 = get(t6,'XData');
+y6 = get(t6,'YData');
+
+h10 = openfig('esercizio_3_punto_4__10_IM.fig');
+t10 = findobj(h10,'type', 'line');
+
+x10 = get(t10,'XData');
+y10 = get(t10,'YData');
+
+figure(5)
+semilogx(x1_05, y1_05, 'b')
+grid on
+hold on
+semilogx(x2, y2, 'r')
+semilogx(x6, y6, 'g')
+semilogx(x10, y10, 'y')
+legend('I_{bias}/I_{th} = 1.05','I_{bias}/I_{th} = 2','I_{bias}/I_{th} = 6','I_{bias}/I_{th} = 10')
+title('Confronto tra le risposte IM per diverse correnti di bias (\epsilon = 1.5e-14)')
+ylabel('Risposta IM [dB]')
+xlabel('Frequenza [GHz]')
+
+%banda come funzione della corrente di bias
+f1_05_3dB_eps = x1_05(6);
+f2_3dB_eps = x2(8);
+f6_3dB_eps = x6(14);
+f10_3dB_eps = x10(20);
+I_th_fix_eps = 9.6672; %mA
+Ibias_eps = [I_th_fix_eps*1.05 I_th_fix_eps*2 I_th_fix_eps*6 I_th_fix_eps*10];
+f_3dB_eps = [f1_05_3dB_eps f2_3dB_eps f6_3dB_eps f10_3dB_eps];
+
+figure(6)
+plot(Ibias_eps, f_3dB_eps, '-o')
+grid on
+
+%Banda di modulazione massima
+%FIXME da rifare con epsilon = 1.5e-14
+n_g = 4.2;
+c = 3e8;
+v_g = c/n_g;
+tau_p = 1/(v_g * (alpha_i_fix * 10^2 + alpha_m_fix * 10^2));
+banda_max_ideale = sqrt(2)/(tau_p * 2 * pi);
