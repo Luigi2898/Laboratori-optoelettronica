1df739be1e543b5f356cbdb934ee421a7e72af50
Luigi <luigi.giuffrida.98@gmail.com>
2020-02-02 18:11:32 +0100

    343

--- Laboratorio 2/diagramma_ad_occhio.m
+++ Laboratorio 2/diagramma_ad_occhio.m
@@ -1,15 +1,15 @@
-clear all;
-close all;
+%clear all;
+%close all;
 clc;
 
-obj = openfig('esercizio_4_NRZ_1');
+obj = openfig('esercizio_4_NRZ_10_1024');
 data = findobj(obj, 'type', 'line');
 
 tempo = get(data(1),'XData'); %ns
 potenza = get(data(1),'YData'); %mW
-bitrate = 1e9; %bit/s
-numb = 128;
-durata = 10^9 * numb / bitrate; %ns
+bitrate =  10e9; %bit/s
+numb = 1024;
+durata = 1e9 * numb / bitrate; %ns
 ini = tempo(numel(tempo)) - durata; %ns
 [M I] = min((tempo - ini).^2);
 tempo = tempo(I : numel(tempo));
@@ -19,4 +19,5 @@ tempo = tempo - tempo(1);
 figure(988)
 plot(tempo,potenza)
 
-eyediagram(potenza, tempo, numb, bitrate, 11)
+eyediagram(potenza, tempo, numb, bitrate, f)
+f = f + 1;
--- Laboratorio 2/eyediagram.m
+++ Laboratorio 2/eyediagram.m
@@ -1,20 +1,21 @@
 function [] = eyediagram(outputpower, time, numb, bitrate, x)
 
-  Tbit = 1e9/bitrate;
-  inter = ceil(Tbit/(time(2) - time(1)));
-  %pause
+  Tbit = 1e9/bitrate
+  time(2) - time(1)
+  inter = floor(Tbit/(time(2) - time(1)))
+  pause
   i = 1;
   for j = 1 : inter : numel(outputpower)
-    if (inter + j < numel(outputpower))
+    if (floor(inter * 1.5) + j < numel(outputpower))
+      j
       figure(x)
-      plot([1 : 1 : floor(1.5 * inter)], outputpower(j : floor(1.5 * inter) + j - 1))
+      plot(time(j : floor(1.5 * inter) + j - 1) - time(j), outputpower(j : floor(1.5 * inter) + j - 1))
       hold on
       i = i + 1
+      pause
     end
   end
   figure(x)
   grid on
 
 end
-
-%FIXME partire dalla transizione forse, non da dove inizia
